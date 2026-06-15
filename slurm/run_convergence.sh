3133444e6d403ff33162b6d7206f96a059b13fba
Kaishya Desai <kaishya@mac.wlan.dur.ac.uk>
2026-06-15 20:17:20 +0100

    Fix Convergence

---
+++ slurm/run_convergence.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --mem=8G
+#SBATCH --time=2:00:00
+#SBATCH -p shared
+#SBATCH --job-name=convergence
+#SBATCH --output=/nobackup/djfb16/morphosim/logs/convergence.out
+#SBATCH --error=/nobackup/djfb16/morphosim/logs/convergence.err
+
+cd /nobackup/djfb16/morphosim
+Rscript run/check_convergence.R
