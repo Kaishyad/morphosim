15077a431de6210dfc1d6e12b31c8f9a2407d13f
Kaishya Desai <kaishya@mac.wlan.dur.ac.uk>
2026-06-25 22:37:54 +0100

    fix convergence

--- slurm/run_convergence.sh
+++ slurm/run_convergence.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 #SBATCH --ntasks=1
 #SBATCH --mem=8G
-#SBATCH --time=2:00:00
+#SBATCH --time=6:00:00
 #SBATCH -p shared
 #SBATCH --job-name=convergence
 #SBATCH --output=/nobackup/djfb16/morphosim/logs/convergence.out
 #SBATCH --error=/nobackup/djfb16/morphosim/logs/convergence.err
 
-
 cd /nobackup/djfb16/morphosim
-Rscript run/check_convergence.R --scenario mk --model model1
+
+for scenario in mk nt; do
+  for model in model1 model2 model3; do
+    Rscript run/check_convergence.R --scenario $scenario --model $model
+  done
+done
 
 cd /nobackup/djfb16/the-matrix
 git add results/convergence_summary.rds results/requeue_list.txt
-git commit -m "Convergence check: mk model1"
+git commit -m "Convergence check: mk+nt model1-3"
 git push origin main
