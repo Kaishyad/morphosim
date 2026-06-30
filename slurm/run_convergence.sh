9452ca54ea40729af5fda54735d03e921d620923
Kaishya Desai <kaishya@mac.wlan.dur.ac.uk>
2026-06-30 10:20:00 +0100

    Edit Convegence run

--- slurm/run_convergence.sh
+++ slurm/run_convergence.sh
@@ -7,15 +7,15 @@
 #SBATCH --output=/nobackup/djfb16/morphosim/logs/convergence.out
 #SBATCH --error=/nobackup/djfb16/morphosim/logs/convergence.err
 
+module load r
+
 cd /nobackup/djfb16/morphosim
 
-for scenario in mk nt; do
-  for model in model1 model2 model3; do
-    Rscript run/check_convergence.R --scenario $scenario --model $model
-  done
+for model in model1 model2 model3 model4 model5 model6 model7 model8 model9 model10 model11 model12; do
+  Rscript run/check_convergence.R --scenario mk --model $model
 done
 
 cd /nobackup/djfb16/the-matrix
 git add results/convergence_summary.rds results/requeue_list.txt
-git commit -m "Convergence check: mk+nt model1-3"
+git commit -m "Convergence check: mk all models"
 git push origin main
