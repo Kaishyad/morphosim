92faa89c07556716838846f43ff209d88b0e024b
Kaishya Desai <kaishya@mac.wlan.dur.ac.uk>
2026-07-07 20:12:45 +0100

    Fixing Visuals

--- viz/run_all.R
+++ viz/run_all.R
@@ -14,7 +14,8 @@ scripts <- c(
   "viz/02_convergence_diagnostics.R",
   "viz/03_parameter_grid_effects.R",
   # "viz/04_imputation_accuracy_plots.R",  # skipped for now -- ignoring imputation
-  "viz/05_summary_dashboard.R"
+  "viz/05_summary_dashboard.R",
+  "viz/06_parameter_grid_by_model.R"
 )
 
 for (s in scripts) {
