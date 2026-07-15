ec38d9d7fb13e8f3903250441cc7f4b8405347ce
Kaishya Desai <kaishya@mac.wlan.dur.ac.uk>
2026-07-15 08:52:21 +0100

    Edit tree comp

--- viz/run_all.R
+++ viz/run_all.R
@@ -13,7 +13,8 @@ scripts <- c(
   # "viz/04_imputation_accuracy_plots.R",  # skipped for now -- ignoring imputation
   "viz/05_summary_dashboard.R",
   "viz/06_parameter_grid_by_model.R",
-  "viz/07_cross_metric_analysis.R"  # requires run/cross_metric_analysis.R to have been run first
+  "viz/07_cross_metric_analysis.R",  # requires run/cross_metric_analysis.R to have been run first
+  "viz/08_tree_similarity_grid.R"
 )
 
 for (s in scripts) {
