49f2db17c524640a6fd05fbe13c105a35e6eada6
Kaishya Desai <kaishya@mac.wlan.dur.ac.uk>
2026-08-06 15:01:39 +0100

    Push fix

--- slurm/push_when_done.sh
+++ slurm/push_when_done.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/config.sh"
 #bash push_when_done.sh <scenario> <model>
 
@@ -14,13 +13,24 @@ fi
 cd "$MATRIX_DIR"
 git checkout "$BRANCH"
 
+FILES=(
+  "${MODEL}_run_1.log"
+  "${MODEL}_run_2.log"
+  "${MODEL}.p_run_1.log"
+  "${MODEL}.p_run_2.log"
+  "${MODEL}_run_1.tar.gz"
+  "${MODEL}_run_2.tar.gz"
+)
+
 for tl in 1.00 1.50 2.50 5.00; do
 
   if [ "$SCENARIO" == "mk" ]; then
     for gl in 0.10 0.25 0.50 1.00; do
       for c in 25 50 100 200; do
         grid="tl${tl}_gl${gl}_c${c}"
-        git add results/${SCENARIO}/${grid}/*/${MODEL}/ 2>/dev/null
+        for f in "${FILES[@]}"; do
+          git add "results/${SCENARIO}/${grid}"/*/"${MODEL}/${f}" 2>/dev/null
+        done
       done
     done
   else
@@ -28,7 +38,9 @@ for tl in 1.00 1.50 2.50 5.00; do
       for pr in 1.00 2.50 5.00; do
         for c in 25 50 100 200; do
           grid="tl${tl}_gl${gl}_pr${pr}_c${c}"
-          git add results/${SCENARIO}/${grid}/*/${MODEL}/ 2>/dev/null
+          for f in "${FILES[@]}"; do
+            git add "results/${SCENARIO}/${grid}"/*/"${MODEL}/${f}" 2>/dev/null
+          done
         done
       done
     done
